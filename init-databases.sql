8a2df8f50a1c0f2c50e476d7f7335c65bc949f92
BENHIMA Mohamed-amine <benhima.mohamed.amine@gmail.com>
2025-12-17 00:42:43 +0100

    set up project

---
+++ init-databases.sql
@@ -0,0 +1,7 @@
+-- Create databases for each microservice
+CREATE DATABASE customer_db;
+CREATE DATABASE inventory_db;
+CREATE DATABASE billing_db;
+CREATE DATABASE beneficiaire_db;
+CREATE DATABASE virement_db;
+CREATE DATABASE chatbot_db;
